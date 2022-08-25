4f7cf67c9cdba0a33de14a161f94755e1c3315a8
Abbas Adel Ibraim <abbas.ibrahim@linus-finance.com>
2022-08-25 15:47:19 +0200

    Using the aggregators in RankingService

--- src/main/java/com/github/abbasadel/perpetua/smartranking/services/RankingService.java
+++ src/main/java/com/github/abbasadel/perpetua/smartranking/services/RankingService.java
@@ -1,7 +1,17 @@
 package com.github.abbasadel.perpetua.smartranking.services;
 
+import com.github.abbasadel.perpetua.smartranking.models.TimeSeries;
 import com.github.abbasadel.perpetua.smartranking.services.providers.SellicsDataProvider;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @Service
 public class RankingService {
@@ -12,4 +22,29 @@ public class RankingService {
         this.sellicsDataProvider = sellicsDataProvider;
     }
 
+    public Flux<TimeSeries> getTimeseriesRankingForAsinByKeyword(final String asin, final String keyword) {
+        return sellicsDataProvider.findRakingByAsinAndKeyword(asin, keyword);
+    }
+
+    public Mono<List<TimeSeries>> getAggregatedTimeseriesRankingForAsin(final String asin, final Optional<Aggregator> aggregator) {
+        var result = sellicsDataProvider.findRakingByAsin(asin);
+        return aggregateResult(result, aggregator);
+    }
+
+    public Mono<List<TimeSeries>> getAggregatedTimeseriesRankingForKeyword(final String keyword, final Optional<Aggregator> aggregator) {
+        var result = sellicsDataProvider.findRakingByKeyword(keyword);
+        return aggregateResult(result, aggregator);
+    }
+
+    private Mono<List<TimeSeries>> aggregateResult(final Flux<TimeSeries> ts, final Optional<Aggregator> aggregator) {
+        if (aggregator.isPresent()) {
+            Collector<TimeSeries, ?, Map<Instant, Double>> collector = Collectors.groupingBy(
+                    TimeSeries::getTimestamp,
+                    aggregator.get().getAggregatorStrategy());
+            return ts.collect(collector)
+                    .map(m -> m.entrySet().stream().map(TimeSeries::of).toList());
+        }
+
+        return ts.collect(Collectors.toList());
+    }
 }
