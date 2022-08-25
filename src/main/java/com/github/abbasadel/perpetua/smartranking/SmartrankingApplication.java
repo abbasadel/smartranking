fb77208a44b4ae6e336576889c8ca5312c49a616
Abbas Adel Ibraim <abbas.ibrahim@linus-finance.com>
2022-08-25 15:40:42 +0200

    initial project setup

---
+++ src/main/java/com/github/abbasadel/perpetua/smartranking/SmartrankingApplication.java
@@ -0,0 +1,13 @@
+package com.github.abbasadel.perpetua.smartranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartrankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartrankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/abbasadel/perpetua/smartranking/SmartrankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.abbasadel.perpetua.smartranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartrankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
